3d6519f07d6f4786e4b0b04a38bd01dd2e159243
oudessyaabw <oudessyaabw@gmail.com>
2021-05-19 11:47:49 +0700

    Menambahkan Login dan Register

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/login');
 });
